671b3c26204d0522e6da734924995a852fca2e6b
Everton Mello <everton.mello.281@gmail.com>
2023-04-04 19:38:24 -0300

    relações funcionando

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -66,3 +66,38 @@ SELECT * FROM users ORDER BY email ASC; -- retorna o resultado ordenado pela col
 SELECT * FROM products ORDER BY price ASC LIMIT 4 OFFSET 0; --retorna o resultado ordenado pela coluna price em ordem crescente, limite o resultado em 4 iniciando pelo primeiro item
 
 SELECT * FROM products WHERE price > 100 AND price < 300 ORDER BY price ASC; -- mocke um intervalo de preços, por exemplo entre 100.00 e 300.00, retorna os produtos com preços dentro do intervalo mockado em ordem crescente.
+
+--Aula Relações sql 1
+-- exercício 1
+DROP TABLE purchases;
+
+CREATE TABLE purchases (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    total_price REAL NOT NULL,
+    paid INTEGER NOT NULL,
+    delivered_at TEXT,
+    buyer_id TEXT NOT NULL,
+    FOREIGN KEY (buyer_id) REFERENCES users(id)
+);
+
+-- exercíco  2
+
+INSERT INTO purchases (id, total_price,paid, delivered_at, buyer_id)
+VALUES
+("1", 300, 0, null, "Arthur"),
+("2", 200, 0, null, "Arthur"),
+("3", 400, 0, null, "Kieffer"),
+("4", 600, 0, datetime('now'), "Kieffer"),
+("5", 100, 0, null, "Matheus"),
+("6", 500, 0, datetime('now'), "Matheus");
+
+UPDATE purchases SET delivered_at = datetime('now') WHERE purchases.id = "1";
+
+SELECT * FROM purchases;
+
+--exercício 3
+
+SELECT * FROM purchases
+INNER JOIN users
+ON purchases.buyer_id = users.id
+WHERE users.id = "Arthur";
